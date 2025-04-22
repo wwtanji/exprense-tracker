1cd11ada018425627d48e529dc04989a42e7d49c
Roman Zotsenko <romazotsenko15@gmail.com>
2025-04-22 21:56:18 +0200

    feat: add AuthLayout component for login form UI and Input component with password visibility toggle

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom';
+
+import Login from "./pages/Auth/Login";
+import SignUp from './pages/Auth/SignUp';
+import Home from "./pages/Dashboard/Home";
+import Income from "./pages/Dashboard/Income";
+import Expense from "./pages/Dashboard/Expense";
 
 const App = () => {
   return (
-    <div class="text-2xl">
-      App
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/signUp" element={<SignUp/>} />
+          <Route path="/dashboard" element={<Home/>} />
+          <Route path="/income" element={<Income/>} />
+          <Route path="/expense" element={<Expense/>} />
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
+const Root = () => {
+  const isAuthenticated = !!localStorage.getItem('token');
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  );
+};
+
