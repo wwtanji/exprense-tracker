722baf38f93009eb5da6c0a7a0a4b6aa895608d7
Roman Zotsenko <romazotsenko15@gmail.com>
2025-04-24 20:42:18 +0200

    Navbar Component

--- frontend/src/pages/Dashboard/Home.jsx
+++ frontend/src/pages/Dashboard/Home.jsx
@@ -5,17 +5,10 @@ import { LuHandCoins, LuWalletMinimal } from "react-icons/lu";
 import { IoMdCard } from "react-icons/io";
 
 import { useNavigate } from "react-router-dom";
-import InfoCard from "../../components/cards/InfoCard";
-import { useUserAuth } from "../../hooks/useUserAuth";
 import axiosInstance from "../../utils/axiosInstance";
 import { API_PATHS } from "../../utils/apiPaths";
 import { addThousandsSeparator } from "../../utils/helper";
-import RecentTransactions from "../../components/Dashboard/RecentTransactions";
-import FinanceOverview from "../../components/Dashboard/FinanceOverview";
-import ExpenseTransactions from "../../components/Dashboard/ExpenseTransactions";
-import Last30DaysExpenses from "../../components/Dashboard/Last30DaysExpenses";
-import RecentIncome from "../../components/Dashboard/RecentIncome";
-import RecentIncomeWithChart from "../../components/Dashboard/RecentIncomeWithChart";
+
 
 const Home = () => {
   useUserAuth();
