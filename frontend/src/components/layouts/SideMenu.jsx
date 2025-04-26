4715979fed11eb8d3ef001f2412f83318ab3b197
Roman Zotsenko <romazotsenko15@gmail.com>
2025-04-26 14:38:47 +0200

    assets for readme documentation and minor fix for side-menu

--- frontend/src/components/layouts/SideMenu.jsx
+++ frontend/src/components/layouts/SideMenu.jsx
@@ -52,7 +52,7 @@ const SideMenu = ({ activeMenu }) => {
           key={`menu_${index}`}
           className={`w-full flex items-center gap-4 text-[15px] ${
             activeMenu == item.label ? "text-white bg-primary" : ""
-          } py-3 px-6 rounded-lg mb-3`}
+          } py-3 px-6 rounded-lg mb-3 cursor-pointer`}
           onClick={() => handleClick(item.path)}
         >
           <item.icon className="text-xl" />
